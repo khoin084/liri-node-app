cda5629eaa6bff2b6fbc7ba29dc1d75a80b4b7b0
Khoi <Khoi@Apples-MacBook-Pro.local>
2017-04-04 23:55:56 -0700

    final release

--- liri.js
+++ liri.js
@@ -6,9 +6,19 @@ var request = require("request");
 var spotify = require("spotify");
 //fs node package for reading and writing files.
 var fs = require("fs");
+//require twiter
+var Twitter = require('twitter');
+
 
 var keyList = fp.twitterKeys;
-console.log(keyList);
+//instantiating twitter obj
+
+var client = new Twitter({
+	consumer_key: keyList.consumer_key,
+	consumer_secret: keyList.consumer_secret,
+	access_token_key: keyList.access_token_key,
+	access_token_secret: keyList.access_token_secret
+});
 
 var nodeArgs = process.argv;
 var caseType = process.argv[2];
@@ -17,35 +27,51 @@ var caseType = process.argv[2];
 nameOfMovie = process.argv.slice(3).join('+');
 nameOfSong = process.argv.slice(3);
 
+//switch case used to identify cli from user.
 switch (caseType) {
 	case "my-tweets":
-		console.log("tweets");
+		readBackTweets();
 		break;
 	case "spotify-this-song":
-		console.log("spotify-this-song");
 		spotifyReq(nameOfSong);
 		break;
 	case "movie-this":
 		movieReq(nameOfMovie);
 		break;
 	case "do-what-it-says":
-		console.log("do-what-it-says");
 		readFs();
 		break;	
 	default:
-		console.log("blank");
+		console.log("blank - default");
+}
+function readBackTweets() {
+	var params = {screen_name: 'KhoiNgu39459148'};
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    console.log("=============================================================================================");
+	    for(var i = 0; i < tweets.length; i++) {
+	    	console.log((parseInt(i) + 1) + "." + tweets[i].text);
+	    }
+	    console.log("=============================================================================================");
+	  }
+	});
 }
 
+//funciton to read file for spotify request
 function readFs() {
+	var dataArr = [];
+	//file system to read and call back with content frome file.
 	fs.readFile("random.txt", "utf8", function(error, data) {
 		if(error) {
 		  	throw error ("error!!!!");
 		} 
-		console.log(data);
-
-		});
+		//splice out the song name and pass to spotifyReq() method.
+		dataArr = data.split(",");
+		spotifyReq(dataArr[1].slice(1, 19));
+	});
 }
-
+//function to call back with spotify request. 
 function spotifyReq(songName) {
 	spotify.search({ type: 'track', query: songName }, function(err, data) {
     if ( err ) {
@@ -60,7 +86,7 @@ function spotifyReq(songName) {
     console.log("=============================================================================================");
 });
 }
-
+//function to achieve call back from omdb api.
 function movieReq (movieName) {
 	request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json", function(error, response, body) {
 	  // If the request is successful (i.e. if the response status code is 200)
